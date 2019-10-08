5e13ba9e34956d1b6d1cb0e10e84b35150b76180
qianbao <599906561@qq.com>
2019-10-08 13:44:24 +0800

    测试开始

--- autologin/Form1.Designer.cs
+++ autologin/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_code = new System.Windows.Forms.Button();
+            this.txt_code = new System.Windows.Forms.TextBox();
+            this.txt_res = new System.Windows.Forms.TextBox();
+            this.txt_cookie = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(177, 259);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(101, 65);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "登录";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.Btn_login_Click);
+            // 
+            // btn_code
+            // 
+            this.btn_code.Location = new System.Drawing.Point(177, 46);
+            this.btn_code.Name = "btn_code";
+            this.btn_code.Size = new System.Drawing.Size(235, 52);
+            this.btn_code.TabIndex = 1;
+            this.btn_code.UseVisualStyleBackColor = true;
+            // 
+            // txt_code
+            // 
+            this.txt_code.Location = new System.Drawing.Point(177, 191);
+            this.txt_code.Name = "txt_code";
+            this.txt_code.Size = new System.Drawing.Size(100, 21);
+            this.txt_code.TabIndex = 2;
+            // 
+            // txt_res
+            // 
+            this.txt_res.Location = new System.Drawing.Point(370, 206);
+            this.txt_res.Multiline = true;
+            this.txt_res.Name = "txt_res";
+            this.txt_res.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_res.Size = new System.Drawing.Size(301, 179);
+            this.txt_res.TabIndex = 3;
+            // 
+            // txt_cookie
+            // 
+            this.txt_cookie.Location = new System.Drawing.Point(460, 118);
+            this.txt_cookie.Name = "txt_cookie";
+            this.txt_cookie.Size = new System.Drawing.Size(100, 21);
+            this.txt_cookie.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_cookie);
+            this.Controls.Add(this.txt_res);
+            this.Controls.Add(this.txt_code);
+            this.Controls.Add(this.btn_code);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_code;
+        private System.Windows.Forms.TextBox txt_code;
+        private System.Windows.Forms.TextBox txt_res;
+        private System.Windows.Forms.TextBox txt_cookie;
     }
 }
 
--- autologin/Form1.cs
+++ autologin/Form1.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using CsharpHttpHelper;
+using CsharpHttpHelper.Enum;
 
 namespace autologin
 {
@@ -14,6 +17,82 @@ namespace autologin
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string refurl = "";
+        private string loginurl = "";
+        private string codeurl = "";
+        private string successurl = "";
+
+        private void Btn_login_Click(object sender, EventArgs e)
+        {
+            var username = "";
+            var pwd = "";
+            var data = string.Format("username={0}&password={1}&verify={2}", username, pwd, txt_code.Text);
+            MessageBox.Show(data);
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem()
+            {
+                URL = loginurl,
+                Encoding = System.Text.Encoding.GetEncoding("utf-8"),
+                Method = "get",
+                IsToLower = false,
+                Cookie = txt_cookie.Text,
+                Referer = refurl,
+                Postdata = data,
+                Timeout = 100000,
+                ReadWriteTimeout = 30000,
+                UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0",
+                ContentType = "application/x-www-form-urlencoded",
+                Allowautoredirect = false,
+                ResultType = ResultType.String
+            };
+            item.Header.Add("X-Requested-With", "XMLHttpRequest");
+
+            HttpResult result = http.GetHtml(item);
+
+            txt_res.Text = result.Html;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem()
+            {
+                URL = refurl,//URL     必需项   
+                Encoding = System.Text.Encoding.GetEncoding("utf-8"),//URL     可选项 默认为Get  
+                Method = "get",//URL     可选项 默认为Get  
+                Referer = refurl,//来源URL     可选项  
+                Allowautoredirect = false,//是否根据301跳转     可选项   
+                ProxyIp = "",//代理服务器ID     可选项 不需要代理 时可以不设置这三个参数   
+            };
+            HttpResult result = http.GetHtml(item);
+
+            txt_cookie.Text = result.Cookie;
+            item = new HttpItem()
+            {
+                URL = codeurl,//URL     必需项   
+                Encoding = System.Text.Encoding.GetEncoding("utf-8"),//URL     可选项 默认为Get  
+                Method = "get",//URL     可选项 默认为Get  
+                Referer = refurl,//来源URL     可选项  
+                Allowautoredirect = false,//是否根据301跳转     可选项  
+                ResultType = ResultType.Byte,
+                Cookie = result.Cookie
+            };
+            result = http.GetHtml(item);
+            btn_code.BackgroundImage = byteArrayToImage(result.ResultByte);
+        }
+
+        /// <summary>
+        /// 字节数组生成图片
+        /// </summary>
+        /// <param name="Bytes"></param>
+        /// <returns></returns>
+        private Image byteArrayToImage(byte[] Bytes)
+        {
+            MemoryStream ms = new MemoryStream(Bytes);
+            Image outputImg = Image.FromStream(ms);
+            return outputImg;
         }
     }
 }
